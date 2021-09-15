2f427ad1e64b28c2fcd1c68fe3077c0532c129b0
RonAviles <89873733+RonAviles@users.noreply.github.com>
2021-09-15 14:41:38 -0500

    cleaned JS file

--- js/site.js
+++ js/site.js
@@ -4,50 +4,36 @@ function getValues() {
     let principal = document.getElementById("loanAmount").value;
     let months = document.getElementById("numOfPayments").value;
     let interest = document.getElementById("intRate").value;
-
     // calculate total monthly payment
     let pmtForm = (interest / 1200) / (1 - (1 + interest / 1200) ** -months);
     let monthlyPmt = principal * pmtForm;
-
     //round monthly payment to 2 decimal point
     let monthlyPmtRounded = monthlyPmt.toFixed(2);
-
     //calculate total cost
     let totalCost = monthlyPmt * months;
-
     //round total cost to 2 decimal points
     let totalCostRounded = totalCost.toFixed(2);
-
     //calculate total interest
     let totalInt = totalCost - principal;
-
     //round total interest to 2 deminal points 
     let totalIntRounded = totalInt.toFixed(2);
-
     //format values to currency and then print to the display;
     var formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
     });
-
     let monthlyFormatted = formatter.format(monthlyPmtRounded);
     let princFormatted = formatter.format(principal);
     let intFormatted = formatter.format(totalIntRounded);
     let totalCostFormatted = formatter.format(totalCostRounded);
-
     document.getElementById("totalMonthly").innerHTML = monthlyFormatted;
-
     document.getElementById("totalPrincipal").innerHTML = princFormatted;
-
     document.getElementById("totalInt").innerHTML = intFormatted;
-
     document.getElementById("totalCost").innerHTML = totalCostFormatted;
-
     CalculatePmts(principal, monthlyPmtRounded, interest, months);
-
 }
 
-// Logic Function 
+// Logic and display function 
 function CalculatePmts(principal, monthly, rate, numOfMonths) {
     //take in total loan amount
     //take in monthly payment
@@ -65,7 +51,6 @@ function CalculatePmts(principal, monthly, rate, numOfMonths) {
         let intPmtRounded = intPmt.toFixed(2);
         let totalIntRounded = totalInt.toFixed(2);
         let remainingBalanceRounded = remainingBalance.toFixed(2);
-
         //inject the elements into a 6 col table 
         document.getElementById("tableBody").innerHTML += `<tr>
         <td>${monthCounter}</td>
@@ -75,7 +60,7 @@ function CalculatePmts(principal, monthly, rate, numOfMonths) {
         <td>${totalIntRounded}</td>
         <td>${remainingBalanceRounded}</td>
         </tr>`;
-
+        //increment month counter and subtract payment from the balance
         balance = remainingBalance;
         monthCounter += 1;
     }
